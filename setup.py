267803f34c3d6fac04263a03af62c4bb67e9d103
Deependra Verma <deependra.verma00@gmail.com>
2024-03-10 05:34:20 +0530

    Requirements Updated

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from typing import List
 
 HYPEN_E_DOT = "-e ."
-def get_requirements(file_path:str)->List[str]:
-    reuirements = []
+def get_requirements(file_path:str)-> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
@@ -21,5 +22,5 @@ setup(
     author = "DeependraVerma",
     author_email = "deependra.verma00@gmail.com",
     install_requires = get_requirements("requirements_dev.txt"),
-    package = find_packages()
+    packages=find_packages()
 )
